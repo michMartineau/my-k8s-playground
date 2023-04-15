ab60beb976dc76f285e0c2d22888c1b37e0f13f4
MiM <michel.t.martineau@gmail.com>
2023-04-15 22:31:50 +0200

    init eks

--- terraform/network.tf
+++ terraform/network.tf
@@ -2,3 +2,21 @@
 resource "aws_vpc" "eks-network" {
   cidr_block = "10.0.0.0/16"
 }
+
+resource "aws_subnet" "example1" {
+  vpc_id     = aws_vpc.eks-network.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
+
+resource "aws_subnet" "example2" {
+  vpc_id     = aws_vpc.eks-network.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,7 +7,6 @@ terraform {
   }
 }
 
-
 provider "aws" {
-  region = "us-east-1"
+  region = "eu-west-3"
 }
